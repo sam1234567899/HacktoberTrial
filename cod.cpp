71b5b07af6a3d8fdf2dfa380dce20e3a3871d538
sudhanshu6324 <34400757+sudhanshu6324@users.noreply.github.com>
2019-10-17 22:14:22 +0530

    code of the day
    cdscdsc csdcds

---
+++ cod.cpp
@@ -0,0 +1,224 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod                   1000000007
+#define ll                    long long
+#define Author                std::ios_base::sync_with_stdio(0);
+#define NIKHIL_SETHI          cout.tie(0);
+#define MNNIT_ALLAHABAD       cin.tie(0);
+#define umii                  unordered_map<ll,ll>
+#define mii                   map<ll,ll>
+#define pii                   pair<ll,ll>
+#define all(v)                v.begin(),v.end()
+#define forP(i,j,k)           for(i=j; i<k; i++)
+#define forN(i,j,k)           for(i=j; i>k; i--)
+#define pb(a)                 push_back(a)
+#define lb(v,t)               lower_bound(all(v),t)-v.begin()
+#define ub(v,t)               upper_bound(all(v),t)-v.begin()
+#define inf 1e18
+ 
+//mainly used functions
+bool prime[10000001];
+ll abst(ll a){if(a<0)return -a; return a;}
+ll power(ll a, ll b){ll res=1;while(b){if(b&1)res=res*a;a=a*a;b/=2;}return res;}
+ll invmod(ll a){return power(a,mod-2);}
+ll lcm(ll a, ll b){return a*b/__gcd(a,b);}
+ll ncr(ll n, ll r){ll p=1;ll i;for(i=1; i<=r; i++){p*=(n-i+1);p*=i;}return p;}
+void sieve(){ll i;ll j;memset(prime,true,10000000);prime[0]=prime[1]=false;for(i=2; i*i<=10000000; i++){if(prime[i]){for(j=2*i; j<=10000000; j+=i){prime[j]=false;}}}}
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        ll a[n];
+        ll i;
+        for(i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        if(n==1)
+        {   
+            if(a[0]>k)
+                cout<<"NO\n";
+            else
+            {
+                cout<<"YES\n";
+                if(a[0]==-1)
+                    cout<<1<<"\n";
+                else
+                    cout<<a[0]<<"\n";
+            }
+        }
+        else
+        {
+        for(i=0;i<n;i++)
+        {
+            if(i==0)
+            {
+                if(a[i]==-1)
+                {
+                    if(a[i+1]==-1)
+                    {
+                        a[i] = 1;
+                    }
+                    else
+                    {
+                        if(a[i+1]==1)
+                            a[i] = a[i+1]+1;
+                        else
+                            a[i] = a[i+1]-1;
+                    }
+                }
+            }
+            else if(i==n-1)
+            {
+                if(a[i]==-1)
+                {
+                    if(a[i-1]==-1)
+                    {
+                        a[i] = 1;
+                    }
+                    else
+                    {
+                        if(a[i-1]==k)
+                            a[i] = a[i-1]-1;
+                        else
+                            a[i] = a[i-1]+1;
+                    }
+                }
+            }
+            else
+            {
+                if(a[i]==-1)
+                {
+                    if(a[i-1]==-1&&a[i+1]==-1)
+                    {
+                        a[i] = 1;
+                    }
+                    else if(a[i-1]==-1&&a[i+1]!=-1)
+                    {
+                        if(a[i+1]==k)
+                        {
+                            a[i] = k-1;
+                        }
+                        else
+                        {
+                            a[i] = a[i+1]+1;
+                        }
+                    }
+                    else if(a[i-1]!=-1&&a[i+1]==-1)
+                    {
+                        if(a[i-1]==k)
+                        {
+                            a[i] = k-1;
+                        }
+                        else
+                        {
+                            a[i] = a[i-1]+1;
+                        }
+                    }
+                    else
+                    {
+                        if(a[i-1]==a[i+1])
+                        {
+                            if(a[i-1]==k)
+                            {
+                                a[i] = k-1;
+                            }
+                            else
+                            {
+                                a[i] = a[i-1]+1;
+                            }
+                        }
+                        else
+                        {
+                            if(a[i-1]==k)
+                            {
+                                if(a[i+1]==k-1)
+                                {
+                                    a[i] = k-2;
+                                }
+                                else
+                                {
+                                    a[i] = k-1;
+                                }
+                            }
+                            else
+                            {
+                                if(a[i+1]==a[i-1]+1)
+                                {
+                                    if(a[i-1]+2>k)
+                                    {
+                                        a[i] = a[i-1]-1;
+                                    }
+                                    else
+                                    {
+                                        a[i] = a[i-1]+2;
+                                    }
+                                }
+                                else
+                                {
+                                    a[i] = a[i-1]+1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        ll flag = 0;
+        for(i=0;i<n;i++)
+        {
+            if(a[i]>k||a[i]<1)
+            {
+                flag = 1;
+                break;
+            }
+            else
+            {
+                if(i==0)
+                {
+                    if(a[i]==a[i+1])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                else if(i==n-1)
+                {
+                    if(a[i]==a[i-1])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                else
+                {
+                    if(a[i]==a[i-1]||a[i]==a[i+1])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        if(flag==1)
+        {
+            cout<<"NO\n";
+        }
+        else
+        {
+            cout<<"YES\n";
+            for(i=0;i<n;i++)
+            {
+                cout<<a[i]<<" ";
+            }
+            cout<<"\n";
+        }
+        }
+    }
+    return 0;
+}
